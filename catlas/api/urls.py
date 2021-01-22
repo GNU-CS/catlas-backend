3809890dc7932e3c991cdccf02c47db0bff5176c
Hatchling <hatchling3713@gmail.com>
2021-01-22 22:50:02 +0900

    Added Login/out, Registration, Reset without E-mail authentication

--- catlas/api/urls.py
+++ catlas/api/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path, include
 
-from api.views import RegistrationAPI
+from knox.views import LogoutView
+
+from api.views import LoginAPI, RegistrationAPI, ResetAPI
 
 urlpatterns = [
-    path('auth/login/', RegistrationAPI.as_view())
+    path('auth/login/', LoginAPI.as_view()),
+    path('auth/register/', RegistrationAPI.as_view()),
+    path('auth/reset/', ResetAPI.as_view()),
+    path('auth/logout/', LogoutView.as_view())
 ]
--- catlas/api/views.py
+++ catlas/api/views.py
@@ -1,20 +1,57 @@
-from rest_framework.generics import GenericAPIView
+from hashlib import sha256
+from secrets import token_hex
+
+from django.contrib.auth.models import User
+
+from rest_framework import generics
+from rest_framework import status
+
 from rest_framework.response import Response
 
-from api.serializers import CreateMemberSerializer
+from knox.auth import AuthToken
+
+from api import serializers
 
 # Create your views here.
 
-class RegistrationAPI(GenericAPIView):
-    serializer_class = CreateMemberSerializer
+class LoginAPI(generics.GenericAPIView):
+    serializer_class = serializers.LoginUserSerializer
 
     def post(self, request, *args, **kwargs):
-        print(request.data)
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
 
-        return Response(
-            {
-                "response": 'pong'
-            }
-        )
+        user = serializer.validated_data
 
+        return Response({
+            'user': serializers.UserSerializer(user, context=self.get_serializer_context()).data,
+            'token': AuthToken.objects.create(user)[1], # AuthToken.objects.create(user) returns tuple (instance, token)
+        }, status=status.HTTP_200_OK)
+
+class RegistrationAPI(generics.GenericAPIView):
+    serializer_class = serializers.CreateUserSerializer
+
+    def post(self, request, *args, **kwargs):
+        try:
+            request.data['email'] += '@gnu.ac.kr'
+
+            serializer = self.get_serializer(data=request.data)
+            
+            serializer.is_valid(raise_exception=True)
+
+            # Implement E-mail authentication
+
+            user = serializer.save()
+
+            return Response({}, status=status.HTTP_201_CREATED)
+
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+class ResetAPI(generics.GenericAPIView):
+    def post(self, request, *args, **kwargs):
+        email = request.data['email'] + '@gnu.ac.kr'
         
+        # Implement E-mail authentication
+
+        return Response({}, status=status.HTTP_200_OK)
